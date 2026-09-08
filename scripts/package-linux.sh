73e3ec7e13d08e356f85c1c8b448512c93a8977c
Martinity <m4rt1n1ty@gmail.com>
2026-09-07 17:50:16 -0700

    (ci) Re-implemented Icon fix for Linux

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -22,6 +22,11 @@ Icon=radiata
 Type=Application
 Categories=Utility;
 EOF
+if [ -f "ui/assets/app_icon.png" ]; then
+  cp "ui/assets/app_icon.png" "$APPDIR/radiata.png"
+else
+  touch "$APPDIR/radiata.png"
+fi
 OUT="RadiataModdingTool-${VERSION}-linux-${ARCH}.AppImage"
 if command -v appimagetool >/dev/null 2>&1; then
   APPIMAGE_ARCH="${ARCH}"
